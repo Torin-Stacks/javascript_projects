c7aa4155a2632dea0db418cd20c5ffe5261f13ae
Torin-Stacks <o.ajanaku@native.semicolon.africa>
2023-06-08 17:30:26 +0100

    task

--- Introduction/variable.js
+++ Introduction/variable.js
@@ -1,8 +1,39 @@
-var number = 28;
-let age = 34;
-const SCHOOL= "Semicoon Africa"
-number = 30;
-age = 43;
-SCHOOL = "semicolon";
-console.log(age);
-console.log(number);
+// var number = 28;
+// let age = 34;
+// const SCHOOL= "Semicolon Africa"
+// number = 30;
+// age = 43;
+// console.log(age);
+// console.log(number);
+
+// var var1 = 2;
+// var var2 = 4;
+// var var3 = 6;
+
+// var temp = var1;
+// var1 = var2;
+// var2 = var3;
+// var3 = temp;
+
+// console.log(var1)
+// console.log(var2)
+// console.log(var3)
+
+var variableOne = 1;
+var variableTwo = 2;
+
+var a = 2 * (variableOne + variableTwo);
+console.log(a);
+
+var b = variableOne % 10 + variableOne;
+console.log(b);
+
+var c = variableOne ** variableTwo;
+console.log(c);
+
+variableTwo = variableOne + 10;
+variableThree = variableOne - 10;
+variableFour = variableOne % 10;
+
+var d = variableOne/ (variableTwo * variableThree * variableFour);
+console.log(d);
