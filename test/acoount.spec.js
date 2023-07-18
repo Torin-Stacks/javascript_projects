a349092326d4a70baea1eae2bc9ffb3bc2864833
Torin-Stacks <o.ajanaku@native.semicolon.africa>
2023-07-18 14:46:59 +0100

    tests

---
+++ test/acoount.spec.js
@@ -0,0 +1,9 @@
+
+//import { largestOddNumber } from "../Introduction/account.js";
+
+const sum = require("../Introduction/account.js")
+
+test("get the sum ",() => {
+    expect(sum(2,3)).toBe(5);
+}
+)
