0db2c37ec6bd9cc7c1ab54b12f2f3a900a18e219
Torin-Stacks <o.ajanaku@native.semicolon.africa>
2023-07-18 14:39:41 +0100

    sum function

---
+++ Introduction/account.js
@@ -0,0 +1,16 @@
+
+function sum (a,b){
+    // let largestNumber ="";
+    // for(let i = numberInString.length; i >= 0; i--){
+    //     if(Number(numberInString[i]) %2 != 0){
+    //         return numberInString.slice(0,i+1)
+    //     }
+    // }
+    // return "";
+    return a + b;
+}
+
+
+module.exports = sum;
+
+//export {largestOddNumber};
--- Introduction/variable.js
+++ Introduction/variable.js
@@ -19,21 +19,192 @@
 // console.log(var2)
 // console.log(var3)
 
-var variableOne = 1;
-var variableTwo = 2;
+// var variableOne = 1;
+// var variableTwo = 2;
 
-var a = 2 * (variableOne + variableTwo);
-console.log(a);
+// var a = 2 * (variableOne + variableTwo);
+// console.log(a);
 
-var b = variableOne % 10 + variableOne;
-console.log(b);
+// var b = variableOne % 10 + variableOne;
+// console.log(b);
 
-var c = variableOne ** variableTwo;
-console.log(c);
+// var c = variableOne ** variableTwo;
+// console.log(c);
 
-variableTwo = variableOne + 10;
-variableThree = variableOne - 10;
-variableFour = variableOne % 10;
+// variableTwo = variableOne + 10;
+// variableThree = variableOne - 10;
+// variableFour = variableOne % 10;
 
-var d = variableOne/ (variableTwo * variableThree * variableFour);
-console.log(d);
+// var d = variableOne/ (variableTwo * variableThree * variableFour);
+// console.log(d);
+
+//window.alert("i love pizza")
+// let age = "21"
+// age = age + 1 
+// console.log(age)
+//let username = window.prompt("whats your name? ")
+// let prompt = require("prompt-sync")()
+// let name = prompt("whats your name? ")
+// console.log(name)
+
+//userInput
+// let username ;
+// document.getElementById("myButton").onclick = function(){
+//     username=document.getElementById("myText").value
+//     console.log(username)
+// }
+
+//type conversion
+// let prompt = require("prompt-sync")()
+// let age = prompt("whats your age? ")
+// console.log(typeof age)
+// age = age+1
+// console.log("I am ", age, " years old")
+
+// age = Number(age) +1 
+
+// console.log(typeof age)
+// console.log("I am ", age, " years old")
+
+// //age = String(age)
+// let state = Boolean("")
+// //console.log(typeof age)
+// console.log(state)
+
+//const
+// const PI =3.14;
+// let radius;
+// let circumference;
+
+// let prompt = require("prompt-sync")();
+// radius = Number(prompt("Enter a radius: "))
+// circumference = 2* PI * radius^2
+// console.log(circumference)
+
+//Math
+// let x = 9.8
+// let y = 5
+// let z = 0
+// let maximum;
+// let minimum;
+
+// x= Math.round(x)
+// maximum = Math.max(x,y,z)
+// console.log(x)
+// console.log(maximum)
+
+//hypotenus
+// let a;
+// let b;
+// let c;
+// let prompt = require("prompt-sync")()
+// a = Number(prompt("ENter a: "))
+// b = Number(prompt("ENter ab: "))
+// c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2))
+// console.log(c)
+
+//simple counter program
+// let count = 0
+// document.getElementById("decreasebtn").onclick = function(){
+    
+//     if(count > 0){
+//         count+=1;
+//     document.getElementById("label"). innerHTML = count}
+// }
+
+// document.getElementById("resetbtn").onclick = function(){
+// count =0;
+//     document.getElementById("label"). innerHTML = count
+// }
+
+// document.getElementById("increasebtn").onclick = function(){
+//     count+=1
+//     document.getElementById("label"). innerHTML = count
+// }
+
+//random number generator
+// let x = Math.ceil((Math.random()*6) + 1)
+// console.log(x)
+
+// let username = "Torinmo"
+// console.log(username.length)
+// console.log(username.charAt(5))
+// console.log(username.indexOf("m"))
+// //trim(),replaceAll("element you want to replace","what to replace it with")
+
+// let username = "big time"
+// let firstword = username.slice(0,username.indexOf(" "))
+// console.log(firstword)
+// let lastword = username.slice(username.indexOf(" " )+ 1)
+// console.log(lastword)
+
+//method chaining
+
+// let username ="bro"
+// let capitfirstletter = username.slice(0,1).toUpperCase()
+// let capitfirstletter2 = username.charAt(0).toUpperCase()
+// console.log(capitfirstletter)
+// console.log(capitfirstletter2)
+
+// document.getElementById("mybutton).onclick").onclick = function(){
+//     if (document.getElementById("myCheckbox").checked){
+//         console.log("youre subscribed")}
+//      else{"youre not subscribed " }
+
+//     }
+
+// let grade;
+// let prompt = require("prompt-sync")();
+// grade = Number(prompt("enter grade: "))
+// switch(true){
+//     case grade > 100: console.log("you did a great job")
+//     break;
+// }
+
+
+//let word = "leetcode"
+
+// function findIndex(word){
+// for(var index = 0; index < word.length; index++){
+//     for (var index2 = 0; index2 < word.length; index2++){
+//         if(word.charAt(index) == word.charAt(index2) && index!=index2){
+//            break
+//         } else if(index2 == word.length-1) return index
+//     }
+// }
+// return -1
+// }
+
+// console.log(findIndex("loveleetcode"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
